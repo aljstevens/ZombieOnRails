32c314560b429fde39c2108a70b9f63226ade2f5
Alexander Stevens <Alexander Stevens>
2016-07-06 18:27:55 -0400

    First Commit

---
+++ Assets/RayCast.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayCast : MonoBehaviour {
+
+	Ray ray;
+	RaycastHit hit;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void Update() {
+		ray=Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		if(Physics.Raycast(ray,out hit))
+		{
+
+			//if(Input.GetKey(KeyCode.Mouse0) && Reloading == true)
+			if (Input.GetButtonDown ("Fire1"))
+			{
+				if (hit.transform.tag == ("Soldier"))
+				{
+					Debug.Log ("HitFriendly");
+				}
+
+				if (hit.transform.tag == ("Enemy"))
+					{
+						Debug.Log ("LOL");
+					}
+
+			}
+	}
+}
+}
