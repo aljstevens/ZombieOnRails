d8e8784a6090723d4e8e975ca77a1e38e1403374
Alexander Stevens <Alexander Stevens>
2016-07-07 23:02:31 -0400

    Added new room

--- Assets/MoveArrow.cs
+++ Assets/MoveArrow.cs
@@ -5,9 +5,11 @@ public class MoveArrow : MonoBehaviour {
 
 	public GameObject Player;
 	public Transform target;
+	public Transform target2;
 	public GameObject LastAreaClear;
 
 	public bool Used;
+	public float MoveTime=4f;
 
 	// Use this for initialization
 	void Start () {
@@ -17,10 +19,16 @@ public class MoveArrow : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		if (Used == true) 
+		if (Used == true && MoveTime >= 0) 
 		{
 			Player.transform.LookAt (target.transform);
 			Player.transform.position = Vector3.MoveTowards (Player.transform.position, target.transform.position, .1f);
+		}
+
+		if (MoveTime <= 0 && target2 != null)
+		{
+			Player.transform.LookAt (target2.transform);
+			Player.transform.position = Vector3.MoveTowards (Player.transform.position, target2.transform.position, .1f);
 		}
 	}
 
@@ -29,5 +37,13 @@ public class MoveArrow : MonoBehaviour {
 		Used = true;
 		LastAreaClear.SetActive (false);
 		Debug.Log ("Arrowed");
+	}
+
+	void FixedUpdate ()
+	{
+		if (Used == true) 
+		{
+			MoveTime -= Time.deltaTime; 
+		}
 	}
 }
